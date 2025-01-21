186359d76b90369d3c0455977c1120b90aec81d7
avashilling <165050625+avashilling@users.noreply.github.com>
2025-01-21 18:10:32 -0500

    Single car controls functioning

---
+++ RichmondRacer/Assets/Scripts/CarControl.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarControl : MonoBehaviour
+{
+    public float motorTorque = 2000;
+    public float brakeTorque = 2000;
+    public float maxSpeed = 20;
+    public float steeringRange = 30;
+    public float steeringRangeAtMaxSpeed = 10;
+    public float centreOfGravityOffset = -1f;
+
+    WheelControl[] wheels;
+    Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+
+        //move center of gravity up
+        rigidBody.centerOfMass += Vector3.up * centreOfGravityOffset;
+
+        //find all child gameobjects with WheelControl script
+        wheels = GetComponentsInChildren<WheelControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vInput = Input.GetAxis("Vertical");
+        float hInput = Input.GetAxis("Horizontal");
+
+        //calculate current speed in relation to forward direction of car
+        float forwardSpeed = Vector3.Dot(transform.forward, rigidBody.velocity);
+
+        //calculate how close car is to top speed as num from 0-1
+        float speedFactor = Mathf.InverseLerp(0, maxSpeed, forwardSpeed);
+
+        //use that to calc how much torque is available (0 at top speed)
+        float currentMotorTorque = Mathf.Lerp(motorTorque, 0, speedFactor);
+
+        //and calculate how much to steer (less sharp when faster
+        float currentSteerRange = Mathf.Lerp(steeringRange, steeringRangeAtMaxSpeed, speedFactor);
+
+        //check whether user input is in same direction as velocity
+        bool isAccelerating = Mathf.Sign(vInput) == Mathf.Sign(forwardSpeed);
+
+        foreach(var wheel in wheels)
+        {
+            //apply steering to wheels w/ "steerable" enabled
+            if (wheel.steerable)
+            {
+                wheel.WheelCollider.steerAngle = hInput * currentSteerRange;
+            }
+
+            if (isAccelerating)
+            {
+                //apply torque to "mmotorized" wheels
+                if (wheel.motorized)
+                {
+                    wheel.WheelCollider.motorTorque = vInput * currentMotorTorque;
+                }
+                wheel.WheelCollider.brakeTorque = 0;
+            }
+            else
+            {
+                //if user is trying to go in opposite direction, apply brakes
+                wheel.WheelCollider.brakeTorque = Mathf.Abs(vInput) * brakeTorque;
+                wheel.WheelCollider.motorTorque = 0;
+            }
+        }
+    }
+}
